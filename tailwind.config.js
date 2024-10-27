a884a3df2af3d7af18722cbdc7579f3b5a3bb832
IliasVilux <iliasotsman@gmail.com>
2024-10-27 17:50:12 +0100

    Set custom colors in tailwind.config.js, adjust navigation blade and components and set home view

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,30 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
-import forms from '@tailwindcss/forms';
+import defaultTheme from "tailwindcss/defaultTheme";
+import forms from "@tailwindcss/forms";
 
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/views/**/*.blade.php",
     ],
 
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans: ["Figtree", ...defaultTheme.fontFamily.sans],
+            },
+            colors: {
+                black: "#1F1F1F",
+                "dark-black": "#121212",
+                purple: {
+                    DEFAULT: "#8B5BF4",
+                    light: "#A77BFF",
+                },
+                white: "#FFFFFF",
+                gray: {
+                    DEFAULT: "#D1D5DB",
+                },
             },
         },
     },
