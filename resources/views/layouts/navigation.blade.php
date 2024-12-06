de90ac4d6f60271ef01c8fce9d9310ad15835f0a
IliasVilux <iliasotsman@gmail.com>
2024-12-06 20:08:39 +0100

    Add 'films' to navigation header

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,6 +15,9 @@
                     <x-nav-link :href="route('serie.catalog')" :active="request()->routeIs('serie.catalog')">
                         {{ __('Series') }}
                     </x-nav-link>
+                    <x-nav-link :href="route('film.catalog')" :active="request()->routeIs('film.catalog')">
+                        {{ __('Películas') }}
+                    </x-nav-link>
                 </div>
             </div>
 
@@ -81,6 +84,9 @@
         <div class="mx-1 pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('serie.catalog')" :active="request()->routeIs('serie.catalog')">
                 {{ __('Series') }}
+            </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('film.catalog')" :active="request()->routeIs('film.catalog')">
+                {{ __('Películas') }}
             </x-responsive-nav-link>
         </div>
 
