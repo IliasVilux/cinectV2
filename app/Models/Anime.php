16d1c4a94de2fb80c7434ef8e0c025ff2aea9702
IliasVilux <iliasotsman@gmail.com>
2024-12-08 23:28:52 +0100

    Add relationship with 'genre' model

--- app/Models/Anime.php
+++ app/Models/Anime.php
@@ -8,5 +8,12 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Anime extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['trailer_link', 'poster_path', 'name', 'overview', 'top', 'number_of_episodes', 'genre_id'];
+
+    public function genre(): BelongsTo
+    {
+        return $this->belongsTo(Genre::class);
+    }
 }
