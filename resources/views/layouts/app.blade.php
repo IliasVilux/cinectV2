a9a8e65cc0933372d030c15bf0ae6c1cc40bfa56
IliasVilux <iliasotsman@gmail.com>
2024-12-10 19:53:39 +0100

    feat: :sparkles: Integrate laravel-share package into the project

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,6 +14,9 @@
 
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+    
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha256-4+XzXVhsDmqanXGHaHvgh1gMQKX40OUvDEBTu8JcmNs=" crossorigin="anonymous"></script>
+    <script src="{{ asset('js/share.js') }}"></script>
 </head>
 
 <body class="font-sans antialiased min-h-screen">
